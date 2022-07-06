88c9fe9193812c789d308c9e44eea6a2c4235809
Liz Ferraro <elizabeth.k.ferraro@gmail.com>
2022-07-05 21:29:36 -0400

    Initial JS script to increment counting on button click

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ## Created by Per Harald Borgen and Presented by FreeCodeCamp (YouTube)
 Link to Source Material: https://www.youtube.com/watch?v=jS4aFq5-91M
 
-Name of Programmer: Liz Ferraro
+Name of Student: Liz Ferraro
 Date: July 5-X, 2022
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-// document.getElementById("count-el").innerText = 5
-
-// let count = 5
-// count = count + 1
-// console.log(count)
+let countEl = document.getElementById("count-el")
 
 let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+    console.log(count)
+}
+
